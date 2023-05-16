19ae976306c417c44587261a7459e4bd1e41537d
Prem-Subhash <133591294+Prem-Subhash@users.noreply.github.com>
2023-05-16 16:41:30 +0530

    Create addition_of_ two_numbers.cpp

---
+++ addition_of_ two_numbers.cpp
@@ -0,0 +1,12 @@
+//Addition of two numbers
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cout<< "enter two numbers";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum"<<c<<end1;
+    return 0;
+}
